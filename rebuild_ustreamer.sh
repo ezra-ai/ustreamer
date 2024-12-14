fe87f5f31176d443833c0ff11fe59900b90a18cd
Aawez Khan <aawezk786@gmail.com>
2024-12-15 02:47:13 +0530

    Update:echo and git pull added in sh file

--- rebuild_ustreamer.sh
+++ rebuild_ustreamer.sh
@@ -19,6 +19,8 @@ if [ -d "$CLONE_DIR" ]; then
 else
   echo "Cloning repository from $REPO_URL..."
   GIT_SSH_COMMAND="ssh -i \"$GIT_SSH_PATH\"" git clone "$REPO_URL"
+  echo "pull latest code from repository $REPO_URL..."
+  GIT_SSH_COMMAND="ssh -i \"$GIT_SSH_PATH\"" git pull
 fi
 
 # Change to the project directory
@@ -31,9 +33,12 @@ else
 fi
 
 cd src/
+echo "service stop.."
 systemctl stop kvmd.service || echo "Failed to stop kvmd.service."
+echo "kvmd.service stopped.."
 mv /usr/bin/ustreamer /usr/bin/ustreamer_bkp
 cp ustreamer.bin /usr/bin/ustreamer
 mv /usr/bin/ustreamer-dump /usr/bin/ustreamer_dump_bkp
 cp ustreamer-dump.bin /usr/bin/ustreamer-dump
 systemctl start kvmd.service || echo "Failed to start kvmd.service."
+echo "kvmd.service started"
